fe18092975b703acd7548c49783467f0d1149d08
unknown <pittc52@gmail.com>
2021-04-25 15:03:59 -0400

    The code has been changed so that it now is in a class I kept the user input

--- alarmclock.py
+++ alarmclock.py
@@ -1,41 +1,46 @@
 from datetime import datetime
 from playsound import playsound
 
-def validate_time(alarm_time):
-    if len(alarm_time) != 5:
-        return "Invalid time format! Please try again..."
-    else:
-        if int(alarm_time[0:2]) > 24:
-            return "Invalid HOUR format! Please try again..."
-        elif int(alarm_time[3:4]) > 59:
-            return "Invalid MINUTE format! Please try again..."
+class Alarm:
+    def __init__ (self, alarm_time):
+        self.alarm_time = alarm_time
+
+    def validate_time(self):
+        alarm_hour = self.alarm_time[0:2]
+        alarm_min = self.alarm_time[3:5]
+
+        if len(self.alarm_time) != 5:
+            return "Invalid time format! Please try again..."
         else:
-            return "ok"
+            if int(self.alarm_time[0:2]) > 24:
+                return "Invalid HOUR format! Please try again..."
+            elif int(self.alarm_time[3:4]) > 59:
+                return "Invalid MINUTE format! Please try again..."
+            else:
+                return "ok"
 
+    def time_checker(self):
+        while True:
+            now = datetime.now()
 
-if __name__ == "__main__":
-    alarm_time = input("Enter time in 'HH:MM' format (Enter in military time): ")
-    
-    validate = validate_time(alarm_time.lower())
-    if validate != "ok":
-        print(validate)
+            current_hour_and_minute = now.strftime("%H:%M")
+
+            if self.alarm_time == current_hour_and_minute:
+                print("Wake Up!")
+                while True:
+                    playsound('/home/mr-robot/alarm/sounds/funny.mp3')
+
+    def get_time (self):
+        return self.alarm_time
+
+if __name__ == '__main__':
+    alarm_time = input("")
+
+    a = Alarm(alarm_time)
+
+    if a.validate_time() != "ok":
+        print(a.validate_time())
     else:
-        print(f"Setting alarm for {alarm_time}...")
-        
-    alarm_hour = alarm_time[0:2]
-    alarm_min = alarm_time[3:5]
-    
-    while True:
-        now = datetime.now()
+        print("Setting alarm for {}...".format(a.get_time()))
 
-        current_hour_and_minute = now.strftime("%H:%M")
-
-        if alarm_time == current_hour_and_minute:
-            print("Wake Up!")
-            while True:
-                playsound('/home/mr-robot/alarm/sounds/funny.mp3')
-            
-
-    print(validate_time(alarm_time))
-
-                    #break
+    a.time_checker()
