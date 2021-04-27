5dc06a52cc06779e5afa5b8c5c00b5394ed079c4
Chris <pittc52@gmail.com>
2021-04-26 22:37:58 -0400

    I made it so the user can now set multiple alarms and other than that it still has same functionality

--- alarmclock.py
+++ alarmclock.py
@@ -2,42 +2,53 @@ from datetime import datetime
 from playsound import playsound
 
 class Alarm:
-    def __init__ (self, alarm_time):
-        self.alarm_time = alarm_time
-
+    def __init__ (self,alarm_times):
+        self.alarm_time_list = []
+        seperate_times = alarm_times.split(",")
+        for time_stamp in seperate_times:
+            single_alarm_time = time_stamp
+            self.alarm_time_list.append(single_alarm_time)
+    
+    def get_alarms(self):
+        return self.alarm_time_list
+    
     def validate_time(self):
-        if len(self.alarm_time) != 5:
-            return "Invalid time format! Please try again..."
-        else:
-            if int(self.alarm_time[0:2]) > 24:
-                return "Invalid HOUR format! Please try again..."
-            elif int(self.alarm_time[3:4]) > 59:
-                return "Invalid MINUTE format! Please try again..."
+        for time_index in self.alarm_time_list:
+            if len(time_index) != 5:
+                return "Invalid time format! Please try again..."
             else:
-                return "ok"
+                if int(time_index[0:2]) > 24:
+                    return "Invalid HOUR format! Please try again..."
+                elif int(time_index[3:4]) > 59:
+                    return "Invalid MINUTE format! Please try again..."
+                else:
+                    return "ok"
 
     def time_checker(self):
         while True:
             now = datetime.now()
 
             current_hour_and_minute = now.strftime("%H:%M")
-
-            if self.alarm_time == current_hour_and_minute:
-                print("Wake Up!")
-                while True:
+            for time_index in self.alarm_time_list:
+                if time_index == current_hour_and_minute:
+                    print("alarm sound")
+                    x = self.alarm_time_list.index(time_index)
+                    self.alarm_time_list.pop(x)
                     playsound('/home/mr-robot/alarm/sounds/funny.mp3')
 
-    def get_time (self):
-        return self.alarm_time
 
 if __name__ == '__main__':
-    alarm_time = input("Enter what time your alarm should go off in HH:MM format. ")
+    alarm_times = input("Enter a list of times your your alarm should go off in HH:MM format. ")
 
-    a = Alarm(alarm_time)
+    a = Alarm(alarm_times)
+
 
     if a.validate_time() != "ok":
         print(a.validate_time())
     else:
-        print("Setting alarm for {}...".format(a.get_time()))
-
+        times = a.get_alarms()
+        print("Setting alarm for...",end='')
+        for time in times:
+            print("{} ".format(time),end='')
+        
     a.time_checker()
